eec44188a7c42734b7ad5782a0d8412e6d7de6e6
leobartowski <francescodangelo95@gmail.com>
2022-04-27 17:51:47 +0200

    WIP

--- Tracce MPI/Aprile2022/main.c
+++ Tracce MPI/Aprile2022/main.c
@@ -4,8 +4,8 @@
 void readMatrix(char *fileName, int *matrix, int dim1, int dim2) {
     FILE *file = fopen(fileName, "r");
 
-    for (int i = 0; i < dim1 ; ++i) {
-        for (int j = 0; j < dim2 ; ++j) {
+    for (int i = 0; i < dim1; ++i) {
+        for (int j = 0; j < dim2; ++j) {
             fscanf(file, "%d ", &matrix[i * dim2 + j]);
         }
     }
@@ -21,7 +21,6 @@ void printMatrix(int *matrix, int dim1, int dim2, int myRank) {
     }
 }
 
-
 int main(int argc, char *argv[]) {
 
     int myRank, nProc;
@@ -32,28 +31,53 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &nProc);
     MPI_Status status;
+    MPI_Aint lb, extent;
 
     int k = matrixDim / nProc;
 
     MPI_Datatype column, columnBlock;
-    MPI_Type_vector(matrixDim, k, matrixDim, MPI_INT, &column);
-    MPI_Type_create_resized(column, 0, k * (sizeof(int)) * nProc, &columnBlock);
+    MPI_Type_vector(k * matrixDim, 1, nProc, MPI_INT, &column);
+    MPI_Type_create_resized(column, lb, sizeof(int), &columnBlock);
     MPI_Type_commit(&columnBlock);
 
+    int startMatrix[matrixDim][matrixDim];
+
     if (myRank == 0) {
-        int startMatrix[matrixDim][matrixDim];
         readMatrix("mat.txt", &startMatrix[0][0], matrixDim, matrixDim);
         printMatrix(&startMatrix[0][0], matrixDim, matrixDim, myRank);
-        for (int i = 0; i < nProc; ++i) {
-            MPI_Send(&startMatrix[0][0], 1, columnBlock, i, 0, MPI_COMM_WORLD);
-        }
     }
 
     int matrixT[matrixDim][k];
+    MPI_Scatter(&startMatrix[0][0], 1, columnBlock, &matrixT[0][0], k * matrixDim, MPI_INT, 0, MPI_COMM_WORLD);
 
-    MPI_Recv(&matrixT[0][0], k * matrixDim, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+//    printMatrix(&matrixT[0][0], matrixDim, k, myRank);
 
-    printMatrix(&matrixT[0][0], matrixDim, k, myRank);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //! Punto 2
+    int maxVector[matrixDim];
+
+    for (int i = 0; i < matrixDim; ++i) {
+        maxVector[i] = matrixT[i][0];
+        for (int j = 0; j < k; ++j) {
+
+            if (matrixT[i][j] > maxVector[i]) {
+                maxVector[i] = matrixT[i][j];
+            }
+        }
+    }
+//    printMatrix(&maxVector[0], matrixDim, 1, myRank);
+
+    //! Punto 3
+    int vectorV0[nProc];
+
+    // Con la Allgather ogni processo invia il suo valore di vi maxVector[0]
+    // e ogni processo riceve la
+    int valueToSend = maxVector[0];
+    MPI_Allgather(&valueToSend, 1, MPI_INT, &vectorV0, 1, MPI_INT, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    printMatrix(&vectorV0[nProc], nProc, 1, myRank);
 
     MPI_Finalize();
     return 0;
